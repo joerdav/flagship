7c7bb3f5ced3e5e096142ce66bc9a305378982ff
joerdav <joe.davidson.21111@gmail.com>
2022-05-20 15:25:33 +0100

    feat: add throttle capabilities

--- flagship.go
+++ flagship.go
@@ -20,6 +20,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"hash/fnv"
+	"io"
+	"math"
 	"sync"
 	"time"
 
@@ -29,9 +32,16 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-// FeatureStore defines the interface for accessing feature flags from some source.
-type FeatureStore interface {
+// BoolFeatureStore defines the interface for accessing boolean typed feature flags from some source.
+type BoolFeatureStore interface {
 	// Bool returns the state of the feature flag with the key of `key`:
+	// If the feature is missing from the table then always returns false.
+	// Example:
+	// {
+	//     "features": {
+	//         "newFeature": true
+	//     }
+	// }
 	//	if s.Bool(context.Background(), "newfeature") {
 	//		// New Code
 	//	} else {
@@ -40,68 +50,44 @@ type FeatureStore interface {
 	Bool(ctx context.Context, key string) bool
 }
 
+// ThrottleFeatureStore defines the interface for accessing a feature flag that needs bucketing.
+type ThrottleFeatureStore interface {
+	// ThrottleAllow returns whether a given hash key is bucketed.
+	// If the feature is missing from the table then always returns false.
+	// Example:
+	// {
+	//     "throttles": {
+	//         "newThrottleFeature": {
+	//             // whitelist is an optional list of hashes that will always be bucketed.
+	//             "whitelist":[10, 3321],
+	//             // probability is the likelihood that a hash is bucketed as a percentage.
+	//             // value is truncated to 2dp
+	//             "probability": 2.5
+	//         }
+	//     }
+	// }
+	//	if s.ThrottleAllow(context.Background(), "newThrottleFeature", strings.NewReader("some hash")) {
+	//		// New Code
+	//	} else {
+	//		// Old code
+	//	}
+	ThrottleAllow(ctx context.Context, key string, hashKey io.Reader) bool
+	// GetHash returns the hash that would be bucketed in ThrottleAllow:
+	//	h := s.GetHash(context.Background(), "newThrottleFeature", strings.NewReader("some hash")) {
+	GetHash(ctx context.Context, key string, hashKey io.Reader) uint
+}
+
+// FeatureStore is an aggregate interface for accessing all supported types of feature flag.
+type FeatureStore interface {
+	BoolFeatureStore
+	ThrottleFeatureStore
+}
+
 type featureStoreConfig struct {
 	TableName, RecordName, Region string
 	CacheTTL                      time.Duration
 	Client                        *dynamodb.Client
 	Now                           func() time.Time
-}
-
-// Option is a function that can modify internal config.
-type Option func(*featureStoreConfig)
-
-// WithRegion allows modification of the AWS Region in which the dynamo table resides.
-// The default value will rely on the AWS Go SDK to find the correct region.
-//	s, err := flagship.New(context.Background(), flagship.WithRegion("eu-west-1"))
-func WithRegion(region string) Option {
-	return func(fsc *featureStoreConfig) {
-		fsc.Region = region
-	}
-}
-
-// WithClock allows the overriding of the function used to get current time.
-// The default value will be `time.Now`.
-//	s, err := flagship.New(context.Background(), flagship.WithClock(func() time.Time { return time.Time{} }))
-func WithClock(clock func() time.Time) Option {
-	return func(fsc *featureStoreConfig) {
-		fsc.Now = clock
-	}
-}
-
-// WithTableName allows modification of the AWS DynamoDB table name.
-// The default value is "featureFlagStore".
-//	s, err := flagship.New(context.Background(), flagship.WithTableName("feature-table"))
-func WithTableName(tableName string) Option {
-	return func(fsc *featureStoreConfig) {
-		fsc.TableName = tableName
-	}
-}
-
-// WithRecordName allows modification of the AWS DynamoDB partition key.
-// The default value is "features".
-//	s, err := flagship.New(context.Background(), flagship.WithRecordName("features1"))
-func WithRecordName(recordName string) Option {
-	return func(fsc *featureStoreConfig) {
-		fsc.RecordName = recordName
-	}
-}
-
-// WithTTL allows modification of the cache expiry for features.
-// The default value is 30 seconds.
-//	s, err := flagship.New(context.Background(), flagship.WithTTL(1 * time.Hour))
-func WithTTL(ttl time.Duration) Option {
-	return func(fsc *featureStoreConfig) {
-		fsc.CacheTTL = ttl
-	}
-}
-
-// WithClient allows modification of the dynamo client used.
-// The default value is constructed using default credentials.
-//	s, err := flagship.New(context.Background(), flagship.WithClient(client))
-func WithClient(client *dynamodb.Client) Option {
-	return func(fsc *featureStoreConfig) {
-		fsc.Client = client
-	}
 }
 
 // New constructs a new instance of the feature store client.
@@ -139,11 +125,94 @@ func New(ctx context.Context, opts ...Option) (FeatureStore, error) {
 		store:    &ds,
 	}
 	// Initial fetch to check it is working
-	_, err := s.fetch(ctx)
+	_, _, err := s.fetch(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("flagship - failed to fetch features: %w", err)
 	}
 	return &s, nil
+}
+
+type ThrottleConfig struct {
+	// Whitelist is a list of hash results that will always be allowed through the throttle.
+	Whitelist []uint `json:"whitelist,omitempty"`
+	// Probability of a hash result making it through the throttle.
+	Probability float64 `json:"probability,omitempty"`
+}
+type throttleConfigInt struct {
+	ThrottleConfig
+	// Threshold is an integer representation of Probability. Floor(Probability*100)
+	Threshold uint
+}
+
+type featureStore struct {
+	fetchMutex      sync.Mutex
+	cacheTTL        time.Duration
+	expiry          time.Time
+	now             func() time.Time
+	cachedFeatures  features
+	cachedThrottles map[string]*throttleConfigInt
+	store           store
+}
+
+func (s *featureStore) ThrottleAllow(ctx context.Context, key string, hashKey io.Reader) bool {
+	_, ts, err := s.fetch(ctx)
+	if err != nil {
+		return false
+	}
+	t := ts[key]
+	h := s.GetHash(ctx, key, hashKey)
+	if t == nil {
+		return false
+	}
+	for _, wl := range t.Whitelist {
+		if h == wl {
+			return true
+		}
+	}
+	if t.Threshold == 0 {
+		return false
+	}
+	if t.Threshold > 100_00 {
+		return true
+	}
+	return h <= t.Threshold
+
+}
+func (s *featureStore) GetHash(ctx context.Context, key string, hashKey io.Reader) uint {
+	f := fnv.New32a()
+	f.Write([]byte(key))
+	_, _ = io.Copy(f, hashKey)
+	return uint(f.Sum32()) % 100_00
+}
+
+func (s *featureStore) Bool(ctx context.Context, key string) bool {
+	f, _, err := s.fetch(ctx)
+	if err != nil {
+		f = s.cachedFeatures
+	}
+	return f.Bool(key)
+}
+
+func (s *featureStore) fetch(ctx context.Context) (features, map[string]*throttleConfigInt, error) {
+	s.fetchMutex.Lock()
+	defer s.fetchMutex.Unlock()
+	if s.now().Before(s.expiry) {
+		return s.cachedFeatures, s.cachedThrottles, nil
+	}
+	f, t, err := s.store.Load(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	s.expiry = s.now().Add(s.cacheTTL)
+	s.cachedFeatures = f
+	s.cachedThrottles = make(map[string]*throttleConfigInt)
+	for k, th := range t {
+		s.cachedThrottles[k] = &throttleConfigInt{
+			ThrottleConfig: th,
+			Threshold:      uint(math.Floor(th.Probability * 100)),
+		}
+	}
+	return s.cachedFeatures, s.cachedThrottles, nil
 }
 
 type features map[string]interface{}
@@ -154,7 +223,7 @@ func (f features) Bool(s string) bool {
 }
 
 type store interface {
-	Load(context.Context) (features, error)
+	Load(context.Context) (features, map[string]ThrottleConfig, error)
 }
 
 type dynamoStore struct {
@@ -162,7 +231,7 @@ type dynamoStore struct {
 	tableName, record string
 }
 
-func (s *dynamoStore) Load(ctx context.Context) (features, error) {
+func (s *dynamoStore) Load(ctx context.Context) (features, map[string]ThrottleConfig, error) {
 	gio, err := s.client.GetItem(ctx, &dynamodb.GetItemInput{
 		TableName: &s.tableName,
 		Key: map[string]types.AttributeValue{
@@ -170,51 +239,23 @@ func (s *dynamoStore) Load(ctx context.Context) (features, error) {
 		},
 	})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	if len(gio.Item) < 1 {
-		return nil, errors.New("record is empty")
+		return nil, nil, errors.New("record is empty")
 	}
 	var f struct {
-		Features features `json:"features"`
+		Features  features                  `json:"features"`
+		Throttles map[string]ThrottleConfig `json:"throttles"`
 	}
 	err = unmarshalMap(gio.Item, &f)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return f.Features, nil
-}
-
-type featureStore struct {
-	fetchMutex     sync.Mutex
-	cacheTTL       time.Duration
-	expiry         time.Time
-	now            func() time.Time
-	cachedFeatures features
-	store          store
-}
-
-func (s *featureStore) Bool(ctx context.Context, key string) bool {
-	f, err := s.fetch(ctx)
-	if err != nil {
-		f = s.cachedFeatures
+	if f.Throttles == nil {
+		f.Throttles = make(map[string]ThrottleConfig)
 	}
-	return f.Bool(key)
-}
-
-func (s *featureStore) fetch(ctx context.Context) (features, error) {
-	s.fetchMutex.Lock()
-	defer s.fetchMutex.Unlock()
-	if s.now().Before(s.expiry) {
-		return s.cachedFeatures, nil
-	}
-	f, err := s.store.Load(ctx)
-	if err != nil {
-		return nil, err
-	}
-	s.expiry = s.now().Add(s.cacheTTL)
-	s.cachedFeatures = f
-	return s.cachedFeatures, nil
+	return f.Features, f.Throttles, nil
 }
 
 func unmarshalMap(m map[string]types.AttributeValue, out interface{}) error {
--- flagship_test.go
+++ flagship_test.go
@@ -2,6 +2,8 @@ package flagship_test
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -11,142 +13,282 @@ import (
 	"github.com/joerdav/flagship"
 )
 
-func setFlag(key string, value bool, table, record string) error {
-	_, err := testClient.PutItem(context.Background(), &dynamodb.PutItemInput{
+func setThrottle(d *dynamodb.Client, key string, value float64, whitelist []string, table, record string) error {
+	k := &types.AttributeValueMemberM{Value: map[string]types.AttributeValue{
+		"probability": &types.AttributeValueMemberN{Value: fmt.Sprint(value)},
+	}}
+	if len(whitelist) > 0 {
+		k.Value["whitelist"] = &types.AttributeValueMemberNS{Value: whitelist}
+	}
+	_, err := d.PutItem(context.Background(), &dynamodb.PutItemInput{
 		Item: map[string]types.AttributeValue{
 			"_pk": &types.AttributeValueMemberS{Value: record},
-			"features": &types.AttributeValueMemberM{Value: map[string]types.AttributeValue{
-				"someflag": &types.AttributeValueMemberBOOL{Value: value},
-			}},
+			"throttles": &types.AttributeValueMemberM{
+				Value: map[string]types.AttributeValue{
+					key: k,
+				},
+			},
 		},
 		TableName: &table,
 	})
 	return err
 }
 
-func TestNew(t *testing.T) {
-	tableName := createLocalTable(t)
+func setFlag(d *dynamodb.Client, key string, value bool, table, record string) error {
+	_, err := d.PutItem(context.Background(), &dynamodb.PutItemInput{
+		Item: map[string]types.AttributeValue{
+			"_pk": &types.AttributeValueMemberS{Value: record},
+			"features": &types.AttributeValueMemberM{
+				Value: map[string]types.AttributeValue{
+					"someflag": &types.AttributeValueMemberBOOL{Value: value},
+				},
+			},
+		},
+		TableName: &table,
+	})
+	return err
+}
+
+func TestCache(t *testing.T) {
+	t.Parallel()
+	testClient, testRegion, err := newTestClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tableName := createLocalTable(t, testClient)
 	t.Cleanup(func() {
-		deleteLocalTable(t, tableName)
+		deleteLocalTable(t, testClient, tableName)
+	})
+	tests := []struct {
+		name         string
+		ttl          time.Duration
+		expectedBool bool
+	}{
+		{
+			name:         "given cache expires, return new value",
+			ttl:          0,
+			expectedBool: true,
+		},
+		{
+			name:         "given cache does not expire, return old value",
+			ttl:          time.Hour * 2,
+			expectedBool: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			record := uuid.New().String()
+			err := setFlag(testClient, "someflag", false, tableName, record)
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			currentTime := time.Time{}
+			store, err := flagship.New(context.Background(),
+				flagship.WithClient(testClient),
+				flagship.WithTableName(tableName),
+				flagship.WithRecordName(record),
+				flagship.WithRegion(testRegion),
+				flagship.WithTTL(tt.ttl),
+				flagship.WithClock(func() time.Time {
+					return currentTime
+				}))
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			b := store.Bool(context.Background(), "someflag")
+			if b {
+				t.Errorf("expected flag to be false, was true")
+			}
+			err = setFlag(testClient, "someflag", true, tableName, record)
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			currentTime = currentTime.Add(time.Hour)
+			if b := store.Bool(context.Background(), "someflag"); b != tt.expectedBool {
+				t.Errorf("expected flag to be %v, was %v", tt.expectedBool, b)
+			}
+		})
+	}
+}
+
+func TestAllowThrottle(t *testing.T) {
+	testClient, testRegion, err := newTestClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tableName := createLocalTable(t, testClient)
+	t.Cleanup(func() {
+		deleteLocalTable(t, testClient, tableName)
+	})
+	tests := []struct {
+		name           string
+		throttleKey    string
+		probability    float64
+		whitelist      []uint
+		hashValue      string
+		givenKey       string
+		expectedResult bool
+	}{
+		{
+			name:           "given throttleKey does not exist should not allow",
+			throttleKey:    "someFeature",
+			probability:    100,
+			givenKey:       "otherFeature",
+			hashValue:      "an input",
+			expectedResult: false,
+		},
+		{
+			name:           "given throttle probability is 0 always disallow",
+			throttleKey:    "someFeature",
+			probability:    0,
+			givenKey:       "someFeature",
+			hashValue:      "an input",
+			expectedResult: false,
+		},
+		{
+			name:           "given throttle probability is 100 always allow",
+			throttleKey:    "someFeature",
+			probability:    100,
+			givenKey:       "someFeature",
+			hashValue:      "an input",
+			expectedResult: true,
+		},
+		{
+			name:           "given hash is within throttle return true",
+			throttleKey:    "someFeature",
+			probability:    50,
+			givenKey:       "someFeature",
+			hashValue:      "an input",
+			expectedResult: true,
+		},
+		{
+			name:           "given hash is within whitelist return true",
+			throttleKey:    "someFeature",
+			probability:    0,
+			whitelist:      []uint{1898},
+			givenKey:       "someFeature",
+			hashValue:      "an input",
+			expectedResult: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			record := uuid.New().String()
+			var wl []string
+			for _, f := range tt.whitelist {
+				wl = append(wl, fmt.Sprint(f))
+			}
+			err := setThrottle(testClient, tt.throttleKey, tt.probability, wl, tableName, record)
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			store, err := flagship.New(
+				context.Background(),
+				flagship.WithClient(testClient),
+				flagship.WithTableName(tableName),
+				flagship.WithRecordName(record),
+				flagship.WithRegion(testRegion),
+			)
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			r := store.ThrottleAllow(context.Background(), tt.givenKey, strings.NewReader(tt.hashValue))
+			if r != tt.expectedResult {
+				hash := store.GetHash(context.Background(), tt.givenKey, strings.NewReader(tt.hashValue))
+				t.Errorf("expected flag to be %v, was %v. hash: %v", tt.expectedResult, r, hash)
+			}
+		})
+	}
+}
+func TestBool(t *testing.T) {
+	testClient, testRegion, err := newTestClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tableName := createLocalTable(t, testClient)
+	t.Cleanup(func() {
+		deleteLocalTable(t, testClient, tableName)
+	})
+	tests := []struct {
+		name         string
+		flags        types.AttributeValue
+		expectedBool bool
+	}{
+		{
+			name:         "given empty flag, return false",
+			flags:        &types.AttributeValueMemberM{Value: make(map[string]types.AttributeValue)},
+			expectedBool: false,
+		},
+		{
+			name: "given false flag, return false",
+			flags: &types.AttributeValueMemberM{
+				Value: map[string]types.AttributeValue{
+					"someflag": &types.AttributeValueMemberBOOL{Value: false},
+				},
+			},
+			expectedBool: false,
+		},
+		{
+			name: "given true flag, return true",
+			flags: &types.AttributeValueMemberM{
+				Value: map[string]types.AttributeValue{
+					"someflag": &types.AttributeValueMemberBOOL{Value: true},
+				},
+			},
+			expectedBool: true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			record := uuid.New().String()
+			_, err := testClient.PutItem(context.Background(), &dynamodb.PutItemInput{
+				Item: map[string]types.AttributeValue{
+					"_pk":      &types.AttributeValueMemberS{Value: record},
+					"features": tt.flags,
+				},
+				TableName: &tableName,
+			})
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			store, err := flagship.New(
+				context.Background(),
+				flagship.WithClient(testClient),
+				flagship.WithTableName(tableName),
+				flagship.WithRecordName(record),
+				flagship.WithRegion(testRegion),
+			)
+			if err != nil {
+				t.Errorf("unexpected error got %v", err)
+			}
+			b := store.Bool(context.Background(), "someflag")
+			if b != tt.expectedBool {
+				t.Errorf("expected flag to be %v, was %v", tt.expectedBool, b)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	testClient, _, err := newTestClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tableName := createLocalTable(t, testClient)
+	t.Cleanup(func() {
+		deleteLocalTable(t, testClient, tableName)
 	})
 	t.Run("if dynamo connection fails should return error", func(t *testing.T) {
+		t.Parallel()
 		_, err := flagship.New(context.Background(), flagship.WithClient(testClient))
 		if err == nil {
 			t.Errorf("expected an error got %v", err)
-		}
-	})
-	t.Run("if loaded successfully, but blank flag then return false", func(t *testing.T) {
-		r := uuid.New().String()
-		_, err := testClient.PutItem(context.Background(), &dynamodb.PutItemInput{
-			Item: map[string]types.AttributeValue{
-				"_pk":      &types.AttributeValueMemberS{Value: r},
-				"features": &types.AttributeValueMemberM{Value: make(map[string]types.AttributeValue)},
-			},
-			TableName: &tableName,
-		})
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		s, err := flagship.New(context.Background(), flagship.WithClient(testClient), flagship.WithTableName(tableName), flagship.WithRecordName(r), flagship.WithRegion(testRegion))
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		b := s.Bool(context.Background(), "someflag")
-		if b {
-			t.Errorf("expected flag to be false, was true")
-		}
-	})
-	t.Run("if loaded successfully, and flag is true return true", func(t *testing.T) {
-		r := uuid.New().String()
-		err := setFlag("someflag", true, tableName, r)
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		s, err := flagship.New(context.Background(), flagship.WithClient(testClient), flagship.WithTableName(tableName), flagship.WithRecordName(r), flagship.WithRegion(testRegion))
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		b := s.Bool(context.Background(), "someflag")
-		if !b {
-			t.Errorf("expected flag to be true, was false")
-		}
-	})
-	t.Run("if loaded successfully, and flag is false return false", func(t *testing.T) {
-		r := uuid.New().String()
-		err := setFlag("someflag", false, tableName, r)
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		s, err := flagship.New(context.Background(), flagship.WithClient(testClient), flagship.WithTableName(tableName), flagship.WithRecordName(r), flagship.WithRegion(testRegion))
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		b := s.Bool(context.Background(), "someflag")
-		if b {
-			t.Errorf("expected flag to be false, was true")
-		}
-	})
-	t.Run("if response is cached and not expired then return cached value", func(t *testing.T) {
-		r := uuid.New().String()
-		err := setFlag("someflag", false, tableName, r)
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		ct := time.Time{}
-		s, err := flagship.New(context.Background(),
-			flagship.WithClient(testClient),
-			flagship.WithTableName(tableName),
-			flagship.WithRecordName(r),
-			flagship.WithRegion(testRegion),
-			flagship.WithTTL(time.Hour),
-			flagship.WithClock(func() time.Time {
-				return ct
-			}))
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		b := s.Bool(context.Background(), "someflag")
-		if b {
-			t.Errorf("expected flag to be false, was true")
-		}
-		err = setFlag("someflag", true, tableName, r)
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		if s.Bool(context.Background(), "someflag") {
-			t.Errorf("expected flag to be false, was true")
-		}
-	})
-	t.Run("if response is cached and expired then return new value", func(t *testing.T) {
-		r := uuid.New().String()
-		err := setFlag("someflag", false, tableName, r)
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		ct := time.Time{}
-		s, err := flagship.New(context.Background(),
-			flagship.WithClient(testClient),
-			flagship.WithTableName(tableName),
-			flagship.WithRecordName(r),
-			flagship.WithRegion(testRegion),
-			flagship.WithTTL(time.Hour),
-			flagship.WithClock(func() time.Time {
-				return ct
-			}))
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		b := s.Bool(context.Background(), "someflag")
-		if b {
-			t.Errorf("expected flag to be false, was true")
-		}
-		err = setFlag("someflag", true, tableName, r)
-		if err != nil {
-			t.Errorf("unexpected error got %v", err)
-		}
-		ct = ct.Add(2 * time.Hour)
-		if !s.Bool(context.Background(), "someflag") {
-			t.Errorf("expected flag to be true, was false")
 		}
 	})
 }
